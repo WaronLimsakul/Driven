c65714542baecc257fd213ab112c6c9b765349ef
WaronLimsakul <poonro@gmail.com>
2025-03-16 16:11:24 -0400

    Set up htmx script

--- internal/templates/layout_templ.go
+++ internal/templates/layout_templ.go
@@ -31,7 +31,7 @@ func head() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<head><title>Driven</title><meta charset=\"UTF-8\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<head><title>Driven</title><meta charset=\"UTF-8\"><script src=\"/static/scripts/htmx.min.js\"></script>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
