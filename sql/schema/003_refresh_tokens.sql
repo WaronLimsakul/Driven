7ad82e3faf7efd49167768c693c5c0101d50d8da
WaronLimsakul <poonro@gmail.com>
2025-03-20 23:48:10 -0400

    Migrate to db v3 refresh_tokens

---
+++ sql/schema/003_refresh_tokens.sql
@@ -0,0 +1,12 @@
+-- +goose Up
+CREATE TABLE refresh_tokens (
+    token CHAR(64) PRIMARY KEY, -- use 32 bytes (256 bits) = 64 digits in hex
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    user_id UUID REFERENCES users (id) ON DELETE CASCADE,
+    expired_at TIMESTAMP,
+    revoked_at TIMESTAMP
+);
+
+-- +goose Down
+DROP TABLE refresh_tokens;
